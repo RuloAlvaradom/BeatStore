bfc45321ceb8d9fda8e7f09b21b44985a1d38e90
MathySavage <mat.padillag@duocuc.cl>
2025-10-15 22:04:23 -0300

    Add files via upload

---
+++ data/db.js
@@ -0,0 +1,107 @@
+export const db = [
+    {
+        id:1,
+        name: 'GUITARRA ELECTRICA TAGIMA',
+        image:'guitarra1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 329900
+    },
+    {
+        id:2,
+        name: 'Guitarra Acústica Yamaha',
+        image:'guitarra2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 159900
+    },
+    {
+        id:3,
+        name: 'Guitarra Clásica Yamaha C40',
+        image:'guitarra3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 139900
+    },
+    {
+        id:4,
+        name: 'Squier CLASSIC VIBE 70 JAZZ',
+        image:'bajo1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 299900
+    },
+    {
+        id:5,
+        name: 'Bajo Ibanez',
+        image:'bajo2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 249900
+    },
+    {
+        id:6,
+        name: 'Bajo Fender Jazz',
+        image:'bajo3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 189900
+    },
+    {
+        id:7,
+        name: 'Bateria electrica',
+        image:'percusion1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 1299990
+    },
+    {
+        id:8,
+        name: 'Congas LP ASPIRE',
+        image:'percusion2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 899990
+    },
+    {
+        id:9,
+        name: 'PAR BAQUETAS TIMBALES 1/2',
+        image:'percusion3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 899990
+    },
+    {
+        id:10,
+        name: 'Batería Acústica Tama Stagestar',
+        image:'percusion4',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 640990
+    },
+    {
+        id:11,
+        name: 'BONGO LP ASPIRE DE FIBRA 6-3/4',
+        image:'percusion5',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 158990
+    },
+    {
+        id:12,
+        name: 'Set de Platillos Sabian HHX Evolution',
+        image:'percusion6',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 1600000
+    },
+    {
+        id:13,
+        name: 'shaker Meinl',
+        image:'percusion7',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 25000
+    },
+    {
+        id:14,
+        name: 'Baqueta Vic Firth madera 5B',
+        image:'percusion8',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 16900
+    },
+    {
+        id:15,
+        name: 'CORTINA 25B LP449 LP PERCUSION',
+        image:'percusion9',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean varius mattis pellentesque. Ut in leo eget sem pretium aliquam a in libero.',
+        price: 34900
+    }, 
+]
