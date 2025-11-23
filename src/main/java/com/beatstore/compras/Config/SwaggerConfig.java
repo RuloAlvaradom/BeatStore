e8553e5273a4bf7f15c254967ef14ff37eba6db6
Raúl Alvarado <rb.alvarado2004@gmail.com>
2025-11-23 19:04:02 -0300

    Actualizacion microservicios, swaggerConfig

---
+++ src/main/java/com/beatstore/compras/Config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.beatstore.compras.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "BeatStore - Microservicio de Usuarios"
+        )
+)
+
+public class SwaggerConfig {
+}
